833d0c27109c0dc5d62731a29e795cea998b02b6
sreejabavandla <sreejabavandla1@gmail.com>
2025-01-26 11:22:39 +0530

    first commit

---
+++ vite-project/src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food() {
+
+  const food1 = "banana";
+  const food2 = "biryani";
+
+  return(
+    <ul>
+      <li>apple</li>
+      <li>{food1}</li>
+      <li>{food2}</li>
+    </ul>
+  );
+}
+
+export default Food
---
+++ vite-project/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return(
+    <footer>
+
+      <p>&copy; Myreactapp</p>
+
+    </footer>
+  );
+}
+
+export default Footer
